1c128c2ed125a36fb36c87119623f716d2a63b91
Aoyama, Shotaro <aosho235@gmail.com>
2018-01-09 16:09:32 +0900

    up

--- main.js
+++ main.js
@@ -1,26 +1,76 @@
-SCREEN_W = 800
-SCREEN_H = 450
+//=============================================================================
+//   定数
+//=============================================================================
+FPS         = 60
+SCREEN_W    = 800
+SCREEN_H    = 450
 
-let type = "WebGL";
-if (!PIXI.utils.isWebGLSupported()) {
-    type = "canvas";
-}
-PIXI.utils.sayHello(type);
 
-let app = new PIXI.Application({width: SCREEN_W, height: SCREEN_H});
-app.renderer.backgroundColor = 0xffffff;
-document.body.appendChild(app.view);
-
-PIXI.loader
-    .add("img/canon.png")
-    .add("img/spider.png")
-    .load(setup);
-
+//=============================================================================
+//   グローバル変数
+//=============================================================================
 var $canon;
-var $objects = [];
-var $frame = 0;
+var $spiders = [];
 var $mouseX = null;
 var $canonAngle = 0;
+var $gameScene;
+
+class Counter {
+    constructor(frame) {
+        this.initFrame = frame;
+        this.frame = frame;
+    }
+
+    count() {
+        this.frame -= 1;
+    }
+
+    reset() {
+        this.frame = this.initFrame;
+    }
+
+    isFinished() {
+        return this.frame <= 0;
+    }
+}
+
+class Spider {
+    constructor() {
+        this.frame = 0;
+
+        var sprite = new PIXI.Sprite(PIXI.loader.resources["img/spider.png"].texture);
+        sprite.width = 40;
+        sprite.height = 40;
+        sprite.x = (Math.random() * SCREEN_W);
+        sprite.y = (Math.random() * SCREEN_H);
+        this.sprite = sprite;
+
+        this.changeSpeed();
+    }
+
+    changeSpeed() {
+        this.changeSpeedCounter = new Counter(randomInt(msToFrame(1000), msToFrame(2000)));
+        var v = Math.random() * 4;
+        var r = Math.random() * 2 * Math.PI;
+        this.sprite.vx = v * Math.cos(r);
+        this.sprite.vy = v * Math.sin(r);
+    }
+
+    action() {
+        this.sprite.x += this.sprite.vx;
+        this.sprite.y += this.sprite.vy;
+        if (this.sprite.x < 0) this.sprite.x = SCREEN_W;
+        if (this.sprite.x > SCREEN_W) this.sprite.x = 0;
+        if (this.sprite.y < 0) this.sprite.y = SCREEN_W;
+        if (this.sprite.y > SCREEN_W) this.sprite.y = 0;
+
+        this.changeSpeedCounter.count();
+        if (this.changeSpeedCounter.isFinished()) {
+            console.log("Change!");
+            this.changeSpeed();
+        }
+    }
+}
 
 function setup() {
     // add canon
@@ -51,35 +101,25 @@ function setup() {
     let message = new PIXI.Text("Hello Pixi!", style);
     app.stage.addChild(message);
 
+    $spiderSpawnCounter = new Counter(msToFrame(1000));
+
     app.ticker.add(delta => gameLoop(delta));
 
     window.addEventListener("mousemove", onMouseMove, false);
 }
 
 function gameLoop(delta) {
-    if ($frame % 60 == 0) {
-        var spider = new PIXI.Sprite(PIXI.loader.resources["img/spider.png"].texture);
-        spider.width = 40;
-        spider.height = 40;
-        spider.x = (Math.random() * 800);
-        spider.y = (Math.random() * 450);
-        var v = Math.random() * 4;
-        var r = Math.random() * 2 * Math.PI;
-        spider.vx = v * Math.cos(r);
-        spider.vy = v * Math.sin(r);
-        app.stage.addChild(spider);
-        $objects.push(spider);
+    $spiderSpawnCounter.count();
+    if ($spiderSpawnCounter.isFinished()) {
+        var spider = new Spider();
+        app.stage.addChild(spider.sprite);
+        $spiders.push(spider);
+
+        $spiderSpawnCounter.reset();
     }
-    $objects.forEach(function(o) {
-        o.x += o.vx;
-        o.y += o.vy;
-        if (o.x < 0) o.x = SCREEN_W;
-        if (o.x > SCREEN_W) o.x = 0;
-        if (o.y < 0) o.y = SCREEN_W;
-        if (o.y > SCREEN_W) o.y = 0;
+    $spiders.forEach(function(s) {
+        s.action();
     });
-    $canon.rotation = 2 * Math.PI * ($canonAngle / 360);
-    $frame += 1;
 }
 
 function onMouseMove(e) {
@@ -91,11 +131,44 @@ function onMouseMove(e) {
     var dx = x - $mouseX;
     $canonAngle += (dx / 5);
     $canonAngle = clamp(-90, $canonAngle, 90);
+    $canon.rotation = 2 * Math.PI * ($canonAngle / 360);
+
     $mouseX = x;
 }
 
+
+//=============================================================================
+//   ユーティリティ関数
+//=============================================================================
 function clamp(min, val, max) {
     if (val < min) return min;
     if (val > max) return max;
     return val;
 }
+
+function msToFrame(ms) {
+    return FPS * ms / 1000;
+}
+
+function randomInt(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
+}
+
+
+//=============================================================================
+//   main
+//=============================================================================
+let type = "WebGL";
+if (!PIXI.utils.isWebGLSupported()) {
+    type = "canvas";
+}
+PIXI.utils.sayHello(type);
+
+let app = new PIXI.Application({width: SCREEN_W, height: SCREEN_H});
+app.renderer.backgroundColor = 0xffffff;
+document.body.appendChild(app.view);
+
+PIXI.loader
+    .add("img/canon.png")
+    .add("img/spider.png")
+    .load(setup);
