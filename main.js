59eed4d25665a62313696a05be713152213fd6e3
Aoyama, Shotaro <aosho235@gmail.com>
2018-01-09 16:34:45 +0900

    up

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 //=============================================================================
 //   定数
 //=============================================================================
-FPS         = 60        // 基本的にPixiJSでは60FPS固定らしい
-SCREEN_W    = 800
-SCREEN_H    = 450
+const FPS         = 60;        // 基本的にPixiJSでは60FPS固定らしい
+const SCREEN_W    = 800;
+const SCREEN_H    = 450;
+
+const IMG_BULLET = "img/bullet.png";
+const IMG_CANON  = "img/canon.png";
+const IMG_CRASH  = "img/crash2.png";
+const IMG_SPIDER = "img/spider.png";
 
 
 //=============================================================================
 //   グローバル変数
 //=============================================================================
+var $app;               // PIXI.Application
+var $gameScene;         // PIXI.Container
+var $mouseX = -1;
+
 var $canon;
 var $spiders = [];
-var $mouseX = null;
-var $gameScene;
+var $spiderSpawnCounter;
 
 
 //=============================================================================
@@ -52,12 +60,93 @@ function randomInt(min, max) {
 }
 
 
+
+//=============================================================================
+//   システム
+//=============================================================================
+function main() {
+    let type = "WebGL";
+    if (!PIXI.utils.isWebGLSupported()) {
+        type = "canvas";
+    }
+    PIXI.utils.sayHello(type);
+
+    $app = new PIXI.Application({width: SCREEN_W, height: SCREEN_H});
+    $app.renderer.backgroundColor = 0xffffff;
+    document.body.appendChild($app.view);
+
+    PIXI.loader
+        .add(IMG_BULLET)
+        .add(IMG_CANON)
+        .add(IMG_CRASH)
+        .add(IMG_SPIDER)
+        .load(setup);
+}
+
+function setup() {
+    $gameScene = new PIXI.Container();
+    $app.stage.addChild($gameScene);
+
+    // add canon
+    $canon = new Canon();
+    $gameScene.addChild($canon.sprite);
+
+    // add text
+    let style = new PIXI.TextStyle({
+                              fontFamily: "Arial",
+                              fontSize: 36,
+                              fill: "white",
+                              stroke: '#ff3300',
+                              strokeThickness: 4,
+                              dropShadow: true,
+                              dropShadowColor: "#000000",
+                              dropShadowBlur: 4,
+                              dropShadowAngle: Math.PI / 6,
+                              dropShadowDistance: 6,
+    });
+    let message = new PIXI.Text("Hello Pixi!", style);
+    $app.stage.addChild(message);
+
+    $spiderSpawnCounter = new Counter(msToFrame(1000));
+
+    $app.ticker.add(delta => gameLoop(delta));
+
+    window.addEventListener("mousemove", onMouseMove, false);
+}
+
+function gameLoop(delta) {
+    $spiderSpawnCounter.count();
+    if ($spiderSpawnCounter.isFinished()) {
+        // add spider
+        var spider = new Spider();
+        $app.stage.addChild(spider.sprite);
+        $spiders.push(spider);
+
+        $spiderSpawnCounter.reset();
+    }
+    $spiders.forEach(function(s) {
+        s.action();
+    });
+}
+
+function onMouseMove(e) {
+    var x = e.clientX;
+    if ($mouseX == -1) {
+        $mouseX = x;
+        return;
+    }
+    var dx = x - $mouseX;
+    $canon.rotateByDx(dx);
+
+    $mouseX = x;
+}
+
 //=============================================================================
 //   ゲームオブジェクト
 //=============================================================================
 class Canon {
     constructor() {
-        let canonSprite = new PIXI.Sprite(PIXI.loader.resources["img/canon.png"].texture);
+        let canonSprite = new PIXI.Sprite(PIXI.loader.resources[IMG_CANON].texture);
         canonSprite.width = 70;
         canonSprite.height = 70;
         canonSprite.x = (SCREEN_W / 2) - 17;
@@ -83,7 +172,7 @@ class Spider {
     constructor() {
         this.frame = 0;
 
-        var sprite = new PIXI.Sprite(PIXI.loader.resources["img/spider.png"].texture);
+        var sprite = new PIXI.Sprite(PIXI.loader.resources[IMG_SPIDER].texture);
         sprite.width = 40;
         sprite.height = 40;
         sprite.x = (Math.random() * SCREEN_W);
@@ -104,14 +193,13 @@ class Spider {
     action() {
         this.sprite.x += this.sprite.vx;
         this.sprite.y += this.sprite.vy;
-        if (this.sprite.x < 0) this.sprite.x = SCREEN_W;
-        if (this.sprite.x > SCREEN_W) this.sprite.x = 0;
-        if (this.sprite.y < 0) this.sprite.y = SCREEN_W;
-        if (this.sprite.y > SCREEN_W) this.sprite.y = 0;
+        if (this.sprite.x < 0)          this.sprite.x = SCREEN_W;
+        if (this.sprite.x > SCREEN_W)   this.sprite.x = 0;
+        if (this.sprite.y < 0)          this.sprite.y = SCREEN_H;
+        if (this.sprite.y > SCREEN_W)   this.sprite.y = 0;
 
         this.changeSpeedCounter.count();
         if (this.changeSpeedCounter.isFinished()) {
-            console.log("Change!");
             this.changeSpeed();
         }
     }
@@ -119,80 +207,6 @@ class Spider {
 
 
 //=============================================================================
-//   システム
-//=============================================================================
-function setup() {
-    // add canon
-    $canon = new Canon();
-    app.stage.addChild($canon.sprite);
-
-    // add text
-    let style = new PIXI.TextStyle({
-                              fontFamily: "Arial",
-                              fontSize: 36,
-                              fill: "white",
-                              stroke: '#ff3300',
-                              strokeThickness: 4,
-                              dropShadow: true,
-                              dropShadowColor: "#000000",
-                              dropShadowBlur: 4,
-                              dropShadowAngle: Math.PI / 6,
-                              dropShadowDistance: 6,
-    });
-    let message = new PIXI.Text("Hello Pixi!", style);
-    app.stage.addChild(message);
-
-    $spiderSpawnCounter = new Counter(msToFrame(1000));
-
-    app.ticker.add(delta => gameLoop(delta));
-
-    window.addEventListener("mousemove", onMouseMove, false);
-}
-
-function gameLoop(delta) {
-    $spiderSpawnCounter.count();
-    if ($spiderSpawnCounter.isFinished()) {
-        // add spider
-        var spider = new Spider();
-        app.stage.addChild(spider.sprite);
-        $spiders.push(spider);
-
-        $spiderSpawnCounter.reset();
-    }
-    $spiders.forEach(function(s) {
-        s.action();
-    });
-}
-
-function onMouseMove(e) {
-    var x = e.clientX;
-    if ($mouseX == null) {
-        $mouseX = x;
-        return;
-    }
-    var dx = x - $mouseX;
-    $canon.rotateByDx(dx);
-
-    $mouseX = x;
-}
-
-
-//=============================================================================
 //   main
 //=============================================================================
-let type = "WebGL";
-if (!PIXI.utils.isWebGLSupported()) {
-    type = "canvas";
-}
-PIXI.utils.sayHello(type);
-
-let app = new PIXI.Application({width: SCREEN_W, height: SCREEN_H});
-app.renderer.backgroundColor = 0xffffff;
-document.body.appendChild(app.view);
-
-PIXI.loader
-    .add("img/canon.png")
-    .add("img/spider.png")
-    .add("img/bullet.png")
-    .add("img/crash2.png")
-    .load(setup);
+main();
